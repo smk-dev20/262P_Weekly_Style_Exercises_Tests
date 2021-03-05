7a71c463eecc50c832b75ffe1dfd2816bc7b7754
smk-dev20 <smk.dev20@gmail.com>
2021-03-05 15:07:57 -0800

    initial week9 attempt

---
+++ src/Week9_Three.py
@@ -0,0 +1,78 @@
+import sys, string
+import numpy as np
+from collections import Counter
+"""
+Week 9 - Array Programing Style
+Code for Exercise 9 heavily reutilizes Code in tf-03.py 
+References
+https://github.com/crista/exercises-in-programming-style/blob/master/03-arrays/tf-03.py
+https://stackoverflow.com/questions/53839435/is-there-a-numpy-equivalent-to-string-translate
+https://stackoverflow.com/questions/43473736/most-common-2-grams-using-python
+"""
+# Example input: "Hello  World!" 
+characters = np.array([' ']+list(open(sys.argv[1]).read())+[' '])
+# Result: array([' ', 'H', 'e', 'l', 'l', 'o', ' ', ' ', 
+#           'W', 'o', 'r', 'l', 'd', '!', ' '], dtype='<U1')
+
+# Normalize
+characters[~np.char.isalpha(characters)] = ' '
+characters = np.char.upper(characters)
+# Result: array([' ', 'h', 'e', 'l', 'l', 'o', ' ', ' ', 
+#           'w', 'o', 'r', 'l', 'd', ' ', ' '], dtype='<U1')
+
+#Leet characters
+leet = np.array(['4','8','<','D','3','7','[','#','1','J','K','L','M','N','0','P','Q','R','5','7','U','V','W','%','Y','2'])
+
+#print(ord('A'))
+
+### Split the words by finding the indices of spaces
+sp = np.where(characters == ' ')
+# Result: (array([ 0, 6, 7, 13, 14], dtype=int64),)
+# A little trick: let's double each index, and then take pairs
+sp2 = np.repeat(sp, 2)
+# Result: array([ 0, 0, 6, 6, 7, 7, 13, 13, 14, 14], dtype=int64)
+# Get the pairs as a 2D matrix, skip the first and the last
+w_ranges = np.reshape(sp2[1:-1], (-1, 2))
+# Result: array([[ 0,  6],
+#                [ 6,  7],
+#                [ 7, 13],
+#                [13, 14]], dtype=int64)
+# Remove the indexing to the spaces themselves
+w_ranges = w_ranges[np.where(w_ranges[:, 1] - w_ranges[:, 0] > 2)]
+# Result: array([[ 0,  6],
+#                [ 7, 13]], dtype=int64)
+
+# Voila! Words are in between spaces, given as pairs of indices
+words = list(map(lambda r: characters[r[0]:r[1]], w_ranges))
+# Result: [array([' ', 'h', 'e', 'l', 'l', 'o'], dtype='<U1'), 
+#          array([' ', 'w', 'o', 'r', 'l', 'd'], dtype='<U1')]
+# Let's recode the characters as strings
+swords = np.array(list(map(lambda w: ''.join(w).strip(), words)))
+# Result: array(['hello', 'world'], dtype='<U5')
+
+#translate words to leet
+swords = np.char.translate(swords,str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ","48<D37[#1JKLMN0PQR57UVW%Y2"))
+
+# Load stop words
+stop_words =np.char.upper( np.array(list(set(open('../stop_words.txt').read().split(',')))))
+
+#translate stopwords to leet
+stop_words = np.char.translate(stop_words,str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ","48<D37[#1JKLMN0PQR57UVW%Y2"))
+
+#remove stop words
+ns_words = swords[~np.isin(swords, stop_words)]
+
+#Find 2 grams and get their counts
+bigrams = zip(ns_words, ns_words[1:])
+counts = Counter(bigrams)
+
+# Print 5 most frequent 2-grams
+for bi in counts.most_common()[:5]:
+    print(np.array(bi[0]), ' - ',bi[1])
+
+### Finally, count the word occurrences
+uniq, counts = np.unique(ns_words, axis=0, return_counts=True)
+wf_sorted = sorted(zip(uniq, counts), key=lambda t: t[1], reverse=True)
+
+#for w, c in wf_sorted[:25]:
+#    print(w, '-', c)
